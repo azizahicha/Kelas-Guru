ad2791edc301dcc0006c5cdfc9677b48d7c183e5
azizahicha <icha_azizah_33rpl@student.smktelkom-mlg.sch.id>
2024-08-07 14:56:21 +0700

    Add files via upload

---
+++ Guru.java
@@ -0,0 +1,19 @@
+//object Class
+public class Guru {
+    String Guru;
+    int id;
+    String nama;
+    String mapel;
+    String alamat;
+
+    /*Constructor */
+    public Guru() {
+        id = 8;
+        nama = "aulia, diaur, feni, andre, pasha";
+        mapel = "produktif rpl, pjok, matematika, b inggris, produktif rpl";
+        alamat = "malang, malang, malang, malang, malang ";
+
+
+    }
+
+}
